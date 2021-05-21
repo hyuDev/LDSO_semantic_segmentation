4eaa2c537a88f381fd726cf9cec73fce556a747d
hyuDev <81205155+hyuDev@users.noreply.github.com>
2021-05-21 11:43:50 +0900

    Add files via upload

---
+++ convertplymesh.py
@@ -0,0 +1,21 @@
+import open3d as o3d
+import trimesh
+import numpy as np
+
+pcd = o3d.io.read_point_cloud("/home/jang-inhoon-ml1/Desktop/out.ply")
+pcd.estimate_normals()
+
+# estimate radius for rolling ball
+distances = pcd.compute_nearest_neighbor_distance()
+avg_dist = np.mean(distances)
+radius = 1.5 * avg_dist   
+
+mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+           pcd,
+           o3d.utility.DoubleVector([radius, radius * 2]))
+
+# create the triangular mesh with the vertices and faces from open3d
+tri_mesh = trimesh.Trimesh(np.asarray(mesh.vertices), np.asarray(mesh.triangles),
+                          vertex_normals=np.asarray(mesh.vertex_normals))
+
+trimesh.convex.is_convex(tri_mesh)
